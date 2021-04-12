35b7f9b64cdcc0b4e3f9660f9e2235d7fa7ef424
kevinp15 <45862391+kevinp15@users.noreply.github.com>
2021-04-11 22:13:51 -0400

    test

--- source/Note.hx
+++ source/Note.hx
@@ -182,7 +182,7 @@ class Note extends FlxSprite
 
 		if (mustPress)
 		{
-			// The * 0.5 is so that it's easier to hit them too late instead of too early
+			// The * 0.5 is so that it's easier to hit them too late, instead of too early
 			if (strumTime > Conductor.songPosition - Conductor.safeZoneOffset
 				&& strumTime < Conductor.songPosition + (Conductor.safeZoneOffset * 0.5))
 				canBeHit = true;

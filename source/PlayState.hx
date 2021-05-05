bad14816cc49f5584d01fa2cf5b73416b395851a
kevinp15 <KevinDevel3@gmail.com>
2021-05-05 10:37:39 -0400

    Removed GF tutorial camera zoom
    I may reintroduce this code if need be

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1371,7 +1371,7 @@ class PlayState extends MusicBeatState
 
 	function tweenCamIn():Void
 	{
-		FlxTween.tween(FlxG.camera, {zoom: 1.3}, (Conductor.stepCrochet * 4 / 1000), {ease: FlxEase.elasticInOut});
+		//FlxTween.tween(FlxG.camera, {zoom: 1.3}, (Conductor.stepCrochet * 4 / 1000), {ease: FlxEase.elasticInOut});
 	}
 
 	override function openSubState(SubState:FlxSubState)

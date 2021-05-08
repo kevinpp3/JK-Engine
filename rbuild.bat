c4b961707138b37eb8d589986d579061ff1097ac
kevinp15 <KevinDevel3@gmail.com>
2021-05-08 17:52:33 -0400

    Fixed rbuild script to make future releases smaller

--- rbuild.bat
+++ rbuild.bat
@@ -1,7 +1,3 @@
 @echo off
 
-del /s /q .\export\release\windows\bin\assets
-mkdir .\export\release\windows\bin\assets
-xcopy /e /t .\assets .\export\release\windows\bin\assets
-
 lime build windows -release
